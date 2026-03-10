66197db34863aa96d5cbabd815d1648c200d7f55
John Ivey <john.ivey91@gmail.com>
2026-03-09 19:10:55 -0500

    Save all current site changes

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,27 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
+    name: "Steel & Concrete Pros",
+    email: "quote@scllc.pro",
+    phoneForTel: "318-383-0070",
+    phoneFormatted: "(318) 383-0070",
     address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
+        lineOne: "6569 Colquitt Rd",
+        lineTwo: "",
+        city: "Keithville",
+        state: "LA",
+        zip: "71047",
         country: "US",
-        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
+        mapLink: "https://maps.app.goo.gl/i3KDCo38tDTWwe5X6",
     },
     socials: {
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        facebook: "https://www.facebook.com/profile.php?id=61559730231208",
+        instagram: "https://www.instagram.com/steelandconcretepros?igsh=NHRnNDRqOWFzc2Fn",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://www.steelandconcretepros.com",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
+
+
+
+
