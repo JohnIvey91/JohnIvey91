e82f7499ea1c92b88315e6391cf32fa5b19f8ff5
John Ivey <john.ivey91@gmail.com>
2026-03-14 01:59:33 -0500

    Update canonical domain and homepage share image

--- src/_data/client.js
+++ src/_data/client.js
@@ -17,7 +17,7 @@ module.exports = {
         instagram: "https://www.instagram.com/steelandconcretepros?igsh=NHRnNDRqOWFzc2Fn",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.steelandconcretepros.com",
+    domain: "https://steelandconcretepros.netlify.app",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
